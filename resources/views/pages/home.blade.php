138d54207292d1ec20d403675e27eca269b0831f
fznh <faznrpl01@gmail.com>
2023-09-06 15:10:18 +0700

    nih bang udah kuperbaiki tampilan home nya hehehe

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -13,14 +13,20 @@
     @include('partials.transparentNav')
     @include('partials.hero')
     {{-- <script src="{{ asset('js/navbar.js') }}"></script> --}}
-    <div class="pt-10 relative">
+    <div class="pt-10 relative max-md:hidden">
         <div class="w-60 h-20 absolute z-10 bg-slate-950"></div>
         <div class="w-48 h-16 ml-16 mt-9 absolute bg-cyan-500"></div>
     </div>
-    <div class="flex mt-10 relative justify-between">
+    {{-- <div class="flex mt-10 relative justify-between max-lg:mx-20 h-[500px] overflow-hidden">
+        @include('home.titleWithDescripton')
+        @include('home.rightImage')
+    </div> --}}
+    <div
+        class="flex justify-between pl-5 relative top-14 overflow-hidden max-xl:gap-5 max-lg:gap-5 max-lg:mx-5 max-md:flex-col-reverse max-md:justify-self-auto max-md:items-center max-md:static max-md:mt-20 max-lg:p-0">
         @include('home.titleWithDescripton')
         @include('home.rightImage')
     </div>
+
     @include('partials.boxLink')
     @include('partials.portalNews')
     @include('partials.footer')
